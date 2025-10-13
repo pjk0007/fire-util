ba51bf6ba152feba678e4c267039c05a58f29ca3
jkpark <pjk0007@naver.com>
2025-10-13 09:39:58 +0900

    feat: chat participants item 컴포넌트 사용

--- src/components/FireChat/FireChatSettings/FireChatSettingsParticipants.tsx
+++ src/components/FireChat/FireChatSettings/FireChatSettingsParticipants.tsx
@@ -6,40 +6,20 @@ import {
 import { FireChannel } from '@/lib/FireChannel/settings';
 import {
     CHANNEL_HOST_ID_FIELD,
-    CHANNEL_ID_FIELD
+    CHANNEL_ID_FIELD,
 } from '@/lib/FireChannel/settings';
 import { FireUser } from '@/lib/FireAuth/settings';
+import { USER_ID_FIELD } from '@/lib/FireAuth/settings';
 import {
-    USER_EMAIL_FIELD,
-    USER_ID_FIELD
-} from '@/lib/FireAuth/settings';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import {
-    USER_AVATAR_FIELD,
     USER_NAME_FIELD,
-    USER_AVATAR_FALLBACK_URL
 } from '@/lib/FireAuth/settings';
-import Image from 'next/image';
-import { UserX2 } from 'lucide-react';
 import { Card } from '@/components/ui/card';
-import { Badge } from '@/components/ui/badge';
 import { memo } from 'react';
-import { Button } from '@/components/ui/button';
 import { useFireAuth } from '@/components/FireProvider/FireAuthProvider';
 import { toast } from 'sonner';
 import removeUser from '@/lib/FireChannel/api/removeUser';
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
 import FireChatSettingsInviteButton from '@/components/FireChat/FireChatSettings/FireChatSettingsInviteButton';
+import FireChatSettingsParticipantsItem from '@/components/FireChat/FireChatSettings/FireChatSettingsParticipantsItem';
 
 function FireChatSettingsParticipants<
     C extends FireChannel<M, T>,
@@ -87,132 +67,136 @@ function FireChatSettingsParticipants<
                 </h2>
             </div>
 
-            <div className='flex flex-col gap-1'>
+            <div className="flex flex-col gap-1">
                 {sortedParticipants.length === 0 ? (
                     <span className="text-sm text-muted-foreground text-center py-6">
                         {FIRECHAT_LOCALE.SIDEBAR.NO_PARTICIPANTS}
                     </span>
                 ) : (
                     sortedParticipants.map((user) => (
-                        <div
-                            key={user.id}
-                            className="flex items-center justify-between gap-3 p-2 py-2 rounded-lg hover:bg-accent transition-colors w-full"
-                        >
-                            <div className="flex items-center gap-3 max-w-40 md:max-w-max">
-                                <Avatar
-                                    className="w-10 h-10 border shadow-sm"
-                                    style={{
-                                        borderRadius: 16,
-                                    }}
-                                >
-                                    <AvatarImage
-                                        src={user[USER_AVATAR_FIELD]}
-                                        alt={user.name}
-                                        className="object-cover"
-                                    />
-                                    <AvatarFallback className="rounded-none">
-                                        <Image
-                                            src={USER_AVATAR_FALLBACK_URL}
-                                            alt={user.name}
-                                            width={40}
-                                            height={40}
-                                            className="object-cover"
-                                        />
-                                    </AvatarFallback>
-                                </Avatar>
-                                <div className="flex flex-col line-clamp-1">
-                                    <span className="text-base font-medium text-foreground transition-colors line-clamp-1 ">
-                                        {channel?.[CHANNEL_HOST_ID_FIELD] ===
-                                        user[USER_ID_FIELD] ? (
-                                            <Badge className="text-xs py-0.5 px-1 mr-1 bg-accent-foreground">
-                                                {FIRECHAT_LOCALE.HOST}
-                                            </Badge>
-                                        ) : user[USER_ID_FIELD] ===
-                                          me?.[USER_ID_FIELD] ? (
-                                            <Badge className="text-xs py-0.5 px-1 mr-1 bg-accent-foreground">
-                                                {FIRECHAT_LOCALE.ME}
-                                            </Badge>
-                                        ) : null}
+                        <FireChatSettingsParticipantsItem
+                            channel={channel}
+                            user={user}
+                        />
+                        // <div
+                        //     key={user.id}
+                        //     className="flex items-center justify-between gap-3 p-2 py-2 rounded-lg hover:bg-accent transition-colors w-full"
+                        // >
+                        //     <div className="flex items-center gap-3 max-w-40 md:max-w-max">
+                        //         <Avatar
+                        //             className="w-10 h-10 border shadow-sm"
+                        //             style={{
+                        //                 borderRadius: 16,
+                        //             }}
+                        //         >
+                        //             <AvatarImage
+                        //                 src={user[USER_AVATAR_FIELD]}
+                        //                 alt={user.name}
+                        //                 className="object-cover"
+                        //             />
+                        //             <AvatarFallback className="rounded-none">
+                        //                 <Image
+                        //                     src={USER_AVATAR_FALLBACK_URL}
+                        //                     alt={user.name}
+                        //                     width={40}
+                        //                     height={40}
+                        //                     className="object-cover"
+                        //                 />
+                        //             </AvatarFallback>
+                        //         </Avatar>
+                        //         <div className="flex flex-col line-clamp-1">
+                        //             <span className="text-base font-medium text-foreground transition-colors line-clamp-1 ">
+                        //                 {channel?.[CHANNEL_HOST_ID_FIELD] ===
+                        //                 user[USER_ID_FIELD] ? (
+                        //                     <Badge className="text-xs py-0.5 px-1 mr-1 bg-accent-foreground">
+                        //                         {FIRECHAT_LOCALE.HOST}
+                        //                     </Badge>
+                        //                 ) : user[USER_ID_FIELD] ===
+                        //                   me?.[USER_ID_FIELD] ? (
+                        //                     <Badge className="text-xs py-0.5 px-1 mr-1 bg-accent-foreground">
+                        //                         {FIRECHAT_LOCALE.ME}
+                        //                     </Badge>
+                        //                 ) : null}
 
-                                        {user[USER_NAME_FIELD]}
-                                    </span>
-                                    <span className="text-sm text-muted-foreground">
-                                        {user[USER_EMAIL_FIELD]}
-                                    </span>
-                                </div>
-                            </div>
-                            <div>
-                                {/* 추가 액션 버튼이나 상태 표시 등을 여기에 넣을 수 있음 */}
-                                {channel?.[CHANNEL_HOST_ID_FIELD] ===
-                                    me?.[USER_ID_FIELD] &&
-                                    user[USER_ID_FIELD] !==
-                                        me?.[USER_ID_FIELD] && (
-                                        <AlertDialog>
-                                            <AlertDialogTrigger asChild>
-                                                <Button
-                                                    className="text-xs"
-                                                    size="sm"
-                                                    variant="outline"
-                                                    disabled={
-                                                        channel?.[
-                                                            CHANNEL_HOST_ID_FIELD
-                                                        ] !==
-                                                            me?.[
-                                                                USER_ID_FIELD
-                                                            ] ||
-                                                        user[USER_ID_FIELD] ===
-                                                            me?.[USER_ID_FIELD]
-                                                    }
-                                                >
-                                                    {
-                                                        FIRECHAT_LOCALE.SIDEBAR
-                                                            .REMOVE_USER_BUTTON
-                                                    }
-                                                </Button>
-                                            </AlertDialogTrigger>
-                                            <AlertDialogContent>
-                                                <AlertDialogHeader>
-                                                    <AlertDialogTitle>
-                                                        {FIRECHAT_LOCALE.SIDEBAR.REMOVE_PARTICIPANT(
-                                                            user[
-                                                                USER_NAME_FIELD
-                                                            ]
-                                                        )}
-                                                    </AlertDialogTitle>
-                                                    <AlertDialogDescription className="text-sm">
-                                                        {FIRECHAT_LOCALE.SIDEBAR.REMOVE_PARTICIPANT(
-                                                            user[
-                                                                USER_NAME_FIELD
-                                                            ]
-                                                        )}
-                                                    </AlertDialogDescription>
-                                                </AlertDialogHeader>
-                                                <AlertDialogFooter>
-                                                    <AlertDialogCancel>
-                                                        {FIRECHAT_LOCALE.CANCEL}
-                                                    </AlertDialogCancel>
-                                                    <AlertDialogAction asChild>
-                                                        <Button
-                                                            size="sm"
-                                                            variant="destructive"
-                                                            onClick={() => {
-                                                                handleRemoveUser(
-                                                                    user
-                                                                );
-                                                            }}
-                                                        >
-                                                            {
-                                                                FIRECHAT_LOCALE.SIDEBAR
-                                                                    .REMOVE_USER_BUTTON
-                                                            }
-                                                        </Button>
-                                                    </AlertDialogAction>
-                                                </AlertDialogFooter>
-                                            </AlertDialogContent>
-                                        </AlertDialog>
-                                    )}
-                            </div>
-                        </div>
+                        //                 {user[USER_NAME_FIELD]}
+                        //             </span>
+                        //             <span className="text-sm text-muted-foreground">
+                        //                 {user[USER_EMAIL_FIELD]}
+                        //             </span>
+                        //         </div>
+                        //     </div>
+                        //     <div>
+                        //         {/* 추가 액션 버튼이나 상태 표시 등을 여기에 넣을 수 있음 */}
+                        //         {channel?.[CHANNEL_HOST_ID_FIELD] ===
+                        //             me?.[USER_ID_FIELD] &&
+                        //             user[USER_ID_FIELD] !==
+                        //                 me?.[USER_ID_FIELD] && (
+                        //                 <AlertDialog>
+                        //                     <AlertDialogTrigger asChild>
+                        //                         <Button
+                        //                             className="text-xs"
+                        //                             size="sm"
+                        //                             variant="outline"
+                        //                             disabled={
+                        //                                 channel?.[
+                        //                                     CHANNEL_HOST_ID_FIELD
+                        //                                 ] !==
+                        //                                     me?.[
+                        //                                         USER_ID_FIELD
+                        //                                     ] ||
+                        //                                 user[USER_ID_FIELD] ===
+                        //                                     me?.[USER_ID_FIELD]
+                        //                             }
+                        //                         >
+                        //                             {
+                        //                                 FIRECHAT_LOCALE.SIDEBAR
+                        //                                     .REMOVE_USER_BUTTON
+                        //                             }
+                        //                         </Button>
+                        //                     </AlertDialogTrigger>
+                        //                     <AlertDialogContent>
+                        //                         <AlertDialogHeader>
+                        //                             <AlertDialogTitle>
+                        //                                 {FIRECHAT_LOCALE.SIDEBAR.REMOVE_PARTICIPANT(
+                        //                                     user[
+                        //                                         USER_NAME_FIELD
+                        //                                     ]
+                        //                                 )}
+                        //                             </AlertDialogTitle>
+                        //                             <AlertDialogDescription className="text-sm">
+                        //                                 {FIRECHAT_LOCALE.SIDEBAR.REMOVE_PARTICIPANT(
+                        //                                     user[
+                        //                                         USER_NAME_FIELD
+                        //                                     ]
+                        //                                 )}
+                        //                             </AlertDialogDescription>
+                        //                         </AlertDialogHeader>
+                        //                         <AlertDialogFooter>
+                        //                             <AlertDialogCancel>
+                        //                                 {FIRECHAT_LOCALE.CANCEL}
+                        //                             </AlertDialogCancel>
+                        //                             <AlertDialogAction asChild>
+                        //                                 <Button
+                        //                                     size="sm"
+                        //                                     variant="destructive"
+                        //                                     onClick={() => {
+                        //                                         handleRemoveUser(
+                        //                                             user
+                        //                                         );
+                        //                                     }}
+                        //                                 >
+                        //                                     {
+                        //                                         FIRECHAT_LOCALE.SIDEBAR
+                        //                                             .REMOVE_USER_BUTTON
+                        //                                     }
+                        //                                 </Button>
+                        //                             </AlertDialogAction>
+                        //                         </AlertDialogFooter>
+                        //                     </AlertDialogContent>
+                        //                 </AlertDialog>
+                        //             )}
+                        //     </div>
+                        // </div>
                     ))
                 )}
                 {channel?.[CHANNEL_HOST_ID_FIELD] === me?.[USER_ID_FIELD] && (
